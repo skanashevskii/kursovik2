8334c596e62b08de9fc427e8db335a5617722b95
Pirania69 <kanashevsky@mail.ru>
2023-06-29 20:15:56 +0300

    ДЗ часть2

--- src/test/java/com/example/kursovik2/ExaminerServiceImplTest.java
+++ src/test/java/com/example/kursovik2/ExaminerServiceImplTest.java
@@ -6,17 +6,20 @@ import com.example.kursovik2.service.ExaminerServiceImpl;
 import com.example.kursovik2.service.QuestionService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
+@ExtendWith(MockitoExtension.class)
 class ExaminerServiceImplTest {
     private ExaminerServiceImpl examinerService;
 
@@ -27,13 +30,15 @@ class ExaminerServiceImplTest {
     private QuestionService mathQuestionService;
 
     @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
+    public void setUp() {
+
         examinerService = new ExaminerServiceImpl(javaQuestionService, mathQuestionService);
     }
 
+//=================================================================================
     @Test
-    void getQuestions() {
+    public void getQuestions() {
+
         List<Question> javaQuestions = new ArrayList<>();
         javaQuestions.add(new Question("Java Question 1", "Java Answer 1"));
         javaQuestions.add(new Question("Java Question 2", "Java Answer 2"));
